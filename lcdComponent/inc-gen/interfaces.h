4326438359ed7cdca0022a7c5f9c2312a688ac32
Akshay Kulkarni <akshaykulkarniak@gmail.com>
2021-01-06 13:12:43 +0530

    Add files via upload

---
+++ lcdComponent/inc-gen/interfaces.h
@@ -0,0 +1,19 @@
+/*
+ * AUTO-GENERATED interface.h for the lcdComponent component.
+
+ * Don't bother hand-editing this file.
+ */
+
+#ifndef __lcdComponent_COMPONENT_INTERFACE_H_INCLUDE_GUARD
+#define __lcdComponent_COMPONENT_INTERFACE_H_INCLUDE_GUARD
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // __lcdComponent_COMPONENT_INTERFACE_H_INCLUDE_GUARD
